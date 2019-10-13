418ecac59077b8bf686814cfa9d744679e40e3e4
Administrator <admin@example.com>
2019-10-13 23:55:52 +0330

    clean code

--- routes/api.php
+++ routes/api.php
@@ -22,22 +22,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::resource('users','UserController');
 
 
 //Route::post('domains','UserController@crateDomain');
 
 //jwt routes
+Route::resource('users','UserController');
 
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
-Route::get('open', 'DataController@open');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::resource('domains','DomainController');
-    Route::post('domains/{domain}/auth','DomainController@domainAuth');
-    Route::get('user', 'UserController@getAuthenticatedUser');
-    Route::get('closed', 'DataController@closed');
 });
 
 Route::group(['middleware' => ['jwt.verify','domain.verify']], function() {
